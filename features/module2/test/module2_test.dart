d7f7bccf967f36c649cfb5fcd9ee204d341c9c88
Gabriel Peres Bernes <gabriel.p_b@hotmail.com>
2022-12-08 15:47:23 -0300

    update coverage

--- features/module2/test/module2_test.dart
+++ features/module2/test/module2_test.dart
@@ -12,7 +12,7 @@ void main() {
   test('adds one to input values', () {
     final calculator = Calculator();
     expect(calculator.addTwo(2), 4);
-    expect(calculator.addTwo(-7), -7);
+    expect(calculator.addTwo(-7), -5);
     expect(calculator.addTwo(0), 2);
   });
 }
